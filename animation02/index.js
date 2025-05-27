371647b2c7b9a9e214906c273f32999291ab4166
Priyanka Katre <priyankack1707@gmail.com>
2025-05-27 14:33:29 +0000

    home page with animation

--- animation02/index.js
+++ animation02/index.js
@@ -1,16 +1,38 @@
  document.addEventListener('DOMContentLoaded', () => {
-     const item2 = document.querySelector('.item2');
-     const item3 = document.querySelector('.item3');
 
+    const marquee = document.querySelector('.marquee');
+console.log('marquee', marquee)
+        marquee.addEventListener('animationiteration', () => {
+            marquee.style.animation = 'none';
+            marquee.offsetHeight; // Trigger reflow
+            marquee.style.animation = null;
+        });
+    const para1 = document.querySelector('.wrapper2 :first-child');
+    const para2 = document.querySelector('.wrapper2 :last-child');
+    const item3 = document.querySelector('.item3');
+  console.log(para1)
      const observer = new IntersectionObserver((entries) => {
-        console.log(entries)
+      
          entries.forEach((entry) => {
-             if (entry.isIntersecting) {
-                 entry.target.style.animation = 'moveItem 1s ease-in';
-             }
+            if (entry.isIntersecting) {
+                let ele = entry.target;
+                let rect = ele.getBoundingClientRect();
+                console.log('rect', rect)
+                if(entry.target === para1) {
+                 entry.target.style.animation = 'paraAnimateLeft 2s ease';
+                }
+                else if(entry.target === para2) {
+                 entry.target.style.animation = 'paraAnimateRight 2s ease';
+                }
+                else if(entry.target === item3) {
+                 entry.target.style.animation = 'moveItem 2s ease'
+                }
+            }
          });
      });
 
-     observer.observe(item2);
+     observer.observe(para1);
+      observer.observe(para2);
      observer.observe(item3);
+
  });
