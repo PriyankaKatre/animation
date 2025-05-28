17c871d046d4d514207c6a0194b268078f724c7a
Priyanka Katre <priyankack1707@gmail.com>
2025-05-28 16:25:04 +0000

    added more content with animation

--- animation03/index.js
+++ animation03/index.js
@@ -1,4 +1,21 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+
+document.addEventListener('DOMContentLoaded', () => {
+    // const scroll = new LocomotiveScroll({
+    //     el: document.querySelector('.main'),
+    //     smooth: true
+    // });
+    const items = document.querySelectorAll('.item');
+    const imageContainer = document.querySelector('.fixed-image-container')
+    Array.from(items).forEach(item => {
+        const image = item.getAttribute('data-image')
+        item.addEventListener('mouseover', () =>{
+            imageContainer.classList.add('fixed-image-container-visible')
+            imageContainer.style.backgroundImage = `url(${image})`;
+        })
+        item.addEventListener('mouseleave', () =>{
+            imageContainer.classList.remove('fixed-image-container-visible')
+        })        
+    });
+ });
+
+
