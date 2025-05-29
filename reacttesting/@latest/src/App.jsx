014feb5936bc33be3e289807effcc6f0e9fdb8b4
Priyanka Katre <priyankack1707@gmail.com>
2025-05-29 10:55:41 +0000

    jest test cases

---
+++ reacttesting/@latest/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div >
+        <div data-testid='counter-value'>count is {count}</div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          increment
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+         decrement
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p >
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ reacttesting/@latest/src/App.test.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import App from "./App";
+import {fireEvent, render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom'
+
+describe('check counter', () =>{
+    beforeEach(() =>{
+         render(<App />);
+    })
+    it('check if Vite + React', () =>{       
+        expect(screen.getByText('Vite + React')).toBeInTheDocument();
+    })
+    it('test if count is 0', () =>{
+        expect(screen.getByTestId('counter-value').textContent).toBe('count is 0')
+    });
+    it('increment count', () =>{       
+        let button = screen.getByText('increment');
+        fireEvent.click(button)
+        expect(screen.getByTestId('counter-value')).toHaveTextContent('count is 1')
+    })
+    it('check multiple time increment', () =>{
+        const incrementbtn = screen.getByText('increment');
+        fireEvent.click(incrementbtn);
+        fireEvent.click(incrementbtn);
+        expect(screen.getByTestId('counter-value')).toHaveTextContent('count is 2')
+    })
+    it('decrement count', () =>{
+        const button = screen.getByText('decrement');
+        const increment = screen.getByText('increment');
+        fireEvent.click(increment)
+        expect(screen.getByTestId('counter-value')).toHaveTextContent('count is 1')
+        fireEvent.click(button)
+        expect(screen.getByTestId('counter-value')).toHaveTextContent('count is 0');
+    })
+    it('check if multiple time decrement click', () =>{
+        const decrementbtn = screen.getByText('decrement');
+        const increment = screen.getByText('increment');
+        fireEvent.click(increment);
+        fireEvent.click(increment)
+        fireEvent.click(decrementbtn);
+        fireEvent.click(decrementbtn);
+        expect(screen.getByTestId('counter-value')).toHaveTextContent('count is 0')
+    })
+})
